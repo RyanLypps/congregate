22514db70c7f0e2145af008e9c836127f73983e9
RyanLypps <r_lypps@yahoo.com>
2020-05-25 16:30:20 -0700

    Wont let you someone with the same socket id join more than one room

--- server/server.js
+++ server/server.js
@@ -17,13 +17,14 @@ let users = [];
 let connections = [];
 let chatRooms = [];
 let usersInChatRoom = [];
+let inRoom = false;
 
 io.sockets.on('connection', socket => {
-  connections.push(socket);
+  connections.push(socket.id);
   console.log('Connected: %s sockets connected ' + connections.length);
 
-   // Disconnect
-   socket.on('disconnect', data => {
+  // Disconnect
+  socket.on('disconnect', data => {
     chatRooms.splice(chatRooms.indexOf(socket.room), 1);
     console.log('Disconnected: %s chatRooms connected ' + chatRooms.length);
     users.splice(users.indexOf(socket.username), 1);
@@ -35,13 +36,13 @@ io.sockets.on('connection', socket => {
     // User leaves chat
     io.to(socket.room).emit('leftChat', `${socket.username} has left the chat.`);
 
-      // Sends right users in Right in rooms
-      io.to(socket.room).emit('onlineUsers', {
-        users: getUsersInRightChatRooms(socket.room),
-      });
+    // Sends right users in Right in rooms
+    io.to(socket.room).emit('onlineUsers', {
+      users: getUsersInRightChatRooms(socket.room),
+    });
     console.log(`Disconnected: %s users connected to chatrooms: ` + usersInChatRoom.length);
   });
-  
+
   // Send Message
   socket.on('sendMessage', message => {
     io.to(`${socket.room}`).emit('message', { msg: message, user: socket.username });
@@ -49,39 +50,61 @@ io.sockets.on('connection', socket => {
 
   // New User
   socket.on('newUser', chatRoomInfo => {
+  
+    if (usersInChatRoom.filter(users => users.id == socket.id).length == 0) {
 
-    getUsersInRoom(chatRoomInfo.chatRoom, chatRoomInfo.username, socket.id);
+      getUsersInRoom(chatRoomInfo.chatRoom, chatRoomInfo.username, socket.id);
 
-    socket.username = chatRoomInfo.username;
-    users.push(socket.username);
-    console.log('Connected: %s users connected ' + users.length);
+      socket.username = chatRoomInfo.username;
+      users.push(socket.username);
+      console.log('Connected: %s users connected ' + users.length);
+
+      socket.room = chatRoomInfo.chatRoom;
+      chatRooms.push(socket.room);
+      console.log(`Connected to room ${chatRoomInfo.chatRoom}`);
+      socket.join(`${chatRoomInfo.chatRoom}`);
+
+      // Sends right users in Right in rooms
+      io.to(chatRoomInfo.chatRoom).emit('onlineUsers', {
+        users: getUsersInRightChatRooms(chatRoomInfo.chatRoom),
+      });
+      // Welcomes User to Room
+      io.to(socket.id).emit('welcomeToRoom', `Welcome to room ${chatRoomInfo.chatRoom}`);
+
+      // Tells other users that new user has joined
+      socket.to(`${chatRoomInfo.chatRoom}`).emit('hasJoinedRoom', `${socket.username} has joined ${chatRoomInfo.chatRoom}`);
+    }
   });
 
   // Join ChatRoom
-  socket.on('joinChatRoom', chatRoomName => {
-    socket.room = chatRoomName.chatRoom;
-    chatRooms.push(socket.room);
-    console.log(`Connected to room ${chatRoomName.chatRoom}`);
-    socket.join(`${chatRoomName.chatRoom}`);
+  // socket.on('joinChatRoom', chatRoomName => {
+  //   // console.log(usersInChatRoom.filter(users => users.id == socket.id));
+  //   // if (usersInChatRoom.filter(users => users.inRoom == false).length >= 1 ) {
 
-    // Sends right users in Right in rooms
-    io.to(chatRoomName.chatRoom).emit('onlineUsers', {
-      users: getUsersInRightChatRooms(chatRoomName.chatRoom),
-    });
-    // Welcomes User to Room
-    io.to(socket.id).emit('welcomeToRoom', `Welcome to room ${chatRoomName.chatRoom}`);
+  //     socket.room = chatRoomName.chatRoom;
+  //     chatRooms.push(socket.room);
+  //     console.log(`Connected to room ${chatRoomName.chatRoom}`);
+  //     socket.join(`${chatRoomName.chatRoom}`);
 
-    // Tells other users that new user has joined
-    socket.to(`${chatRoomName.chatRoom}`).emit('hasJoinedRoom', `${socket.username} has joined ${chatRoomName.chatRoom}`);
-  });
+  //     // Sends right users in Right in rooms
+  //     io.to(chatRoomName.chatRoom).emit('onlineUsers', {
+  //       users: getUsersInRightChatRooms(chatRoomName.chatRoom),
+  //     });
+  //     // Welcomes User to Room
+  //     io.to(socket.id).emit('welcomeToRoom', `Welcome to room ${chatRoomName.chatRoom}`);
+
+  //     // Tells other users that new user has joined
+  //     socket.to(`${chatRoomName.chatRoom}`).emit('hasJoinedRoom', `${socket.username} has joined ${chatRoomName.chatRoom}`);
+  //   // }
+  // });
 
   let getUsersInRoom = (chatRoomName, username, id) => {
-    let userInRoom = {chatRoomName, username, id};
-      usersInChatRoom.push(userInRoom);
+    let userInRoom = { chatRoomName, username, id, inRoom };
+    usersInChatRoom.push(userInRoom);
   };
 
   let getUsersInRightChatRooms = (chatRoomName) => {
-    return usersInChatRoom.filter(user => user.chatRoomName == chatRoomName); 
+    return usersInChatRoom.filter(user => user.chatRoomName == chatRoomName);
   };
 
 });
